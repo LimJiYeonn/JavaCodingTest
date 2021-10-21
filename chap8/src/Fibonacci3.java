9e7e3b4b92481f34e557f80396fb9fc6040b66bd
JiYeon <jiueon6@gmail.com>
2021-10-21 11:07:17 +0900

    fibo

--- chap8/src/Fibonacci3.java
+++ chap8/src/Fibonacci3.java
@@ -1,11 +1,9 @@
-public class Fibonacci2 {
-    // 탑다운(하향식, 메모이제이션) 방식 : 큰문제 해결위해 작은 문체를 호출
-    // 한 번 계산된 결과를 메모이제이션(Memoization)하기 위한 배열 초기화
+public class Fibonacci3 {
+    //호출되는 함수 확인
     public static long[] d = new long[100];
 
-    // 피보나치 함수(Fibonacci Function)를 재귀함수로 구현 (탑다운 다이나믹 프로그래밍)
     public static long fibo(int x) {
-        // 종료 조건(1 혹은 2일 때 1을 반환)
+        System.out.print("f(" + x + ") ");
         if (x == 1 || x == 2) {
             return 1;
         }
@@ -19,6 +17,6 @@ public class Fibonacci2 {
     }
 
     public static void main(String[] args) {
-        System.out.println(fibo(50));
+        fibo(6);
     }
 }
