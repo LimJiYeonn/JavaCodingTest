060ca17784fd8e5189c28e09dc73d714498e8d34
JiYeon <jiueon6@gmail.com>
2021-12-20 23:53:13 +0900

    update

--- 백준/src/Q2839.java
+++ 백준/src/Q2839.java
@@ -1,17 +1,38 @@
 import java.util.Scanner;
-public class Q2839 {
-    public static void main(String[] args) {
+public class Q2839 { //tistory 보기
+    public static void main(String[] args) { //설탕 배달
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int cnt = 0;
-        
-        cnt = n / 5;
-        n %= 5;
-        if(n % 3 == 0){
-            cnt += n / 3;
-            System.out.println(cnt);
-        }else{
+        int N = sc.nextInt();
+
+        if (N == 4 || N == 7) {
             System.out.println(-1);
         }
+        else if (N % 5 == 0) {
+            System.out.println(N / 5);
+        }
+        else if (N % 5 == 1 || N % 5 == 3) {
+            System.out.println((N / 5) + 1);
+        }
+        else if (N % 5 == 2 || N % 5 == 4) {
+            System.out.println((N / 5) + 2);
+        }
+
+        /*
+        if (n == 4 || n == 7) {
+            System.out.println(-1);
+        }else if(n % 5 == 0){
+            System.out.println(n/5);
+        }
+        else if(n <= 12 && n % 3 == 0){
+            System.out.println(n/3);
+        }else if((n-5) % 3 == 0){
+            System.out.println((n-5)/3+1);
+        }else if((n-10) % 3 == 0){
+            System.out.println((n-10)/3+2);
+        }else if((n-15) % 3 == 0){
+            System.out.println((n-15)/3+3);
+        }
+
+         */
     }
 }
--- 백준/src/Q2869_3.java
+++ 백준/src/Q2869_3.java
@@ -3,7 +3,7 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.StringTokenizer;
 
-public class Q2869_3 {
+public class Q2869_3 { //달팽이는 올라가고 싶다
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
