f1934a5c6634e7fed4246517ab0fed81bc1a46a2
JiYeon <jiueon6@gmail.com>
2021-12-20 22:23:39 +0900

    create

--- 백준/src/Q2839.java
+++ 백준/src/Q2839.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
 public class Q2839 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int cnt = 0;
         
+        cnt = n / 5;
+        n %= 5;
+        if(n % 3 == 0){
+            cnt += n / 3;
+            System.out.println(cnt);
+        }else{
+            System.out.println(-1);
+        }
     }
 }
