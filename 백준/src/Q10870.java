8c9803484df70a4a9dd4a40da21984b548f0aa33
JiYeon <jiueon6@gmail.com>
2021-12-30 21:49:14 +0900

    Create Q10870

---
+++ 백준/src/Q10870.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Q10870 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(fivo(n));
+    }
+    public static int fivo(int n){
+        if(n == 0) return 0;
+        else if(n == 1) return 1;
+        else return fivo(n-1) + fivo(n-2);
+    }
+}
---
+++ 백준/src/Q11729.java
@@ -0,0 +1,5 @@
+public class Q11729 {
+    public static void main(String[] args) {
+        
+    }
+}
