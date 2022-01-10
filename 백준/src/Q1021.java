232697069a0822e207bbc1295c2dccc908535101
JiYeon <jiueon6@gmail.com>
2022-01-10 11:49:18 +0900

    Update Q1021.java

--- 백준/src/Q1021.java
+++ 백준/src/Q1021.java
@@ -1,2 +1,17 @@
-public class Q1021 {
+import java.io.*;
+import java.util.*;
+public class Q1021 { //회전하는 큐 -> 덱으로 구현
+    //큐를 구현클래스는 PriorityQueue(우선순위 큐), ArrayDeque(배열 양방향 큐), LinkedList(연결리스트)
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        Deque<Integer> deque = new LinkedList<>();
+        for(int i = 1; i <= n; i++) {
+
+
+        }
+    }
 }
